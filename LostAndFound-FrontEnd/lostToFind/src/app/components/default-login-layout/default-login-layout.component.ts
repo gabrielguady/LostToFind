416f4bff4d0e5f5f2935b2241349fc559a0cb5f6
gabrielguady <gabrielguady@gmail.com>
2024-11-27 13:54:38 -0400

    commit do login hmtl e css

--- LostAndFound-FrontEnd/lostToFind/src/app/components/default-login-layout/default-login-layout.component.ts
+++ LostAndFound-FrontEnd/lostToFind/src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,11 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-default-login-layout',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    ReactiveFormsModule
   ],
   templateUrl: './default-login-layout.component.html',
   styleUrl: './default-login-layout.component.css'
@@ -14,4 +16,19 @@ export class DefaultLoginLayoutComponent {
   @Input() title: string='';
   @Input() primaryBtnText: string='';
   @Input() secondaryBtnText: string='';
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
 }
