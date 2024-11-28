5cf98089ca16a4eeb8f7ec937cb77fd860d70869
gabrielguady <gabrielguady@gmail.com>
2024-11-28 16:29:35 -0400

    commit de login mudanças

--- LostAndFound-FrontEnd/lostToFind/src/app/components/default-login-layout/default-login-layout.component.ts
+++ LostAndFound-FrontEnd/lostToFind/src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-default-login-layout',
@@ -16,19 +16,4 @@ export class DefaultLoginLayoutComponent {
   @Input() title: string='';
   @Input() primaryBtnText: string='';
   @Input() secondaryBtnText: string='';
-
-  loginForm: FormGroup;
-
-  constructor(private fb: FormBuilder) {
-    this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
-    });
-  }
-
-  onSubmit(): void {
-    if (this.loginForm.valid) {
-      console.log(this.loginForm.value);
-    }
-  }
 }
--- LostAndFound-FrontEnd/lostToFind/src/app/pages/login/login.component.ts
+++ LostAndFound-FrontEnd/lostToFind/src/app/pages/login/login.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
-import {HeaderComponent} from '../../components/header/header.component';
+import {Component, OnInit} from '@angular/core';
 import {DefaultLoginLayoutComponent} from '../../components/default-login-layout/default-login-layout.component';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    DefaultLoginLayoutComponent
+    DefaultLoginLayoutComponent,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  public loginForm! : FormGroup;
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      email : new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    })
+  }
 
 }
