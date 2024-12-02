b1c7acec631c83ee83ea0330383f46972b3b2698
gabrielguady <gabrielguady@gmail.com>
2024-12-02 16:35:58 -0400

    commit service

---
+++ LostAndFound-FrontEnd/lostToFind/src/app/components/service.component.ts
@@ -0,0 +1,17 @@
+import {NavigationExtras, Router} from '@angular/router';
+import {AppService} from '../../shared/services/app.service'
+import {HttpClient} from '@angular/common/http';
+
+export class BaseComponent<T> {
+  private router: Router = new Router();
+  public service: AppService<T>;
+
+  constructor(http: HttpClient, url: string) {
+    this.service = new AppService<T>(http, url);
+  }
+
+  public goToPage(route: string): void {
+    const extras: NavigationExtras = {queryParamsHandling: 'merge'};
+    this.router.navigate([route], extras).then();
+  }
+}
