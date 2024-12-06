002478e98a5349bdd52c73078ee45170e6c3877b
valery <@valery>
2024-12-05 21:15:01 -0400

    primeiro commit add components ao home

--- LostAndFound-FrontEnd/lostToFind/src/app/pages/home/home.component.ts
+++ LostAndFound-FrontEnd/lostToFind/src/app/pages/home/home.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from './header/header.component';
+import {BannerComponent} from './banner/banner.component';
+import {CategoriesComponent} from './categories/categories.component';
+import {RecentItemsComponent} from './recent-items/recent-items.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    BannerComponent,
+    CategoriesComponent,
+    RecentItemsComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  lostItems: string[] = [];
+  foundItems: string[] = [];
 
+  constructor() {
+
+    this.lostItems = ['Chave', 'Celular', 'Carteira'];
+    this.foundItems = ['Óculos', 'Relógio', 'Livro'];
+  }
+
+  addLostItem(item: string) {
+    this.lostItems.push(item);
+  }
+
+  addFoundItem(item: string) {
+    this.foundItems.push(item);
+  }
 }
